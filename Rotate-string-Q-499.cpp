5e2446b0002d683ef1ee9cf0886ff78b1a5b84cd
Gaurav Kumar <sinha4545@gmail.com>
2023-06-25 18:59:27 +0530

    Rotate String Question 499

---
+++ Rotate-string-Q-499.cpp
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/rotate-string/
+#include<string>
+using namespace std;
+class Solution {
+public:
+    bool rotateString(string s, string goal)
+    {
+        if (s.size() != goal.size())
+        {
+            return false;
+        }
+        if ((s + s).find(goal) != string::npos)
+            return true;
+
+        return false;
+    }
+};
