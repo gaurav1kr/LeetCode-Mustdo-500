065cadb542f8c83d733ce6fa336a84038bb5c0b7
Gaurav Kumar <sinha4545@gmail.com>
2023-06-25 17:08:39 +0530

    Unique Email Addresses

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # LeetCode-Mustdo-500
 The Solution of Must do 500 DSA questions
+--- All References are in  Coding-Interview-cheatsheet.xlsx
---
+++ email-search-Q-500.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+#include<string>
+using namespace std;
+
+class Solution {
+public:
+    int numUniqueEmails(vector<string>& emails)
+    {
+        unordered_map<string, int> freqMap;
+        for (string str : emails)
+        {
+            RemovePlus(str);
+            ++freqMap[str];
+        }
+        return freqMap.size();
+    }
+
+    void RemovePlus(string& str)
+    {
+        size_t pos_plus = str.find("+");
+        size_t pos_atrate = str.find("@");
+        if ((pos_plus != string::npos) && (pos_atrate != string::npos) && (pos_atrate > pos_plus))
+        {
+            str.erase(str.begin() + pos_plus, str.begin() + pos_atrate);
+            pos_atrate = str.find('@');
+        }
+
+        for (size_t i = 0; i < pos_atrate; i++)
+        {
+            if (str[i] == '.')
+            {
+                str.erase(str.begin() + i);
+            }
+        }
+    }
+};
