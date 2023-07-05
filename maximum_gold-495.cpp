7557fc577d85e69007959e64595a159cf5dd5386
Gaurav Kumar <sinha4545@gmail.com>
2023-07-05 23:07:40 +0530

    Max gold collection - 495

--- maximum_gold-495.cpp
+++ maximum_gold-495.cpp
@@ -11,12 +11,20 @@ public:
     Solution() : x_axis{ 0,-1,0,1 }, y_axis{ 1,0,-1,0 }
     {
     }
-    void check_max(int i, int j, vector<vector<bool>> visited, vector<vector<int>> grid, int row_grid, int col_grid, int  sum)
+    bool check_equal_elem(vector<vector<int>> grid , int row_grid , int col_grid)
     {
-        if (i < 0 || i >= row_grid || j < 0 || j >= col_grid || !grid[i][j] || visited[i][j])
-            return;
-        visited[i][j] = true;
+        int source = grid[0][0];
+        for(int i=0;i<row_grid;i++)
+        for (int j = 1; j < col_grid; j++)
+        {
+            if (grid[i][j] != source) return false;
+        }
+        return true;
+    }
+    void check_max(int i, int j, vector<vector<int>> grid, int row_grid, int col_grid, int  sum)
+    {
         sum += grid[i][j];
+        grid[i][j] = -grid[i][j];
         if (sum > max_gold)
         {
             max_gold = sum;
@@ -24,16 +32,25 @@ public:
 
         for (int k = 0; k < AXIS; k++)
         {
-            check_max(i + x_axis[k], j + y_axis[k], visited, grid, row_grid, col_grid, sum);
+            int new_x = i + x_axis[k];
+            int new_y = j + y_axis[k];
+            if (new_x >= 0 && new_y >= 0 && new_x < row_grid && new_y < col_grid && grid[new_x][new_y] > 0)
+                check_max(new_x, new_y, grid, row_grid, col_grid, sum);
         }
-        visited[i][j] = false;
+        grid[i][j] = -grid[i][j];
     }
     int getMaximumGold(vector<vector<int>>& grid)
     {
         int row_grid = grid.size();
         int col_grid = grid[0].size();
         max_gold = 0;
-        vector<vector<bool>> visited(row_grid, vector<bool>(col_grid, false));
+
+        bool equal_elem = check_equal_elem(grid, row_grid, col_grid);
+        if (equal_elem)
+        {
+            return row_grid * col_grid * grid[0][0];
+        }
+
         for (int i = 0; i < row_grid; i++)
         {
             for (int j = 0; j < col_grid; j++)
@@ -41,10 +58,23 @@ public:
                 if (grid[i][j])
                 {
                     int sum = 0;
-                    check_max(i, j, visited, grid, row_grid, col_grid, sum);
+                    check_max(i, j, grid, row_grid, col_grid, sum);
                 }
             }
         }
         return max_gold;
     }
 };
+
+int main()
+{
+    vector<vector<int>> grid = { {1,1,1,1,1},
+                                 {1,1,1,1,1},
+                                 {1,1,1,1,1},
+                                 {1,1,1,1,1},
+                                 {1,1,1,1,1} 
+                               };
+    Solution sol;
+    cout << sol.getMaximumGold(grid);
+    return 0;
+}
