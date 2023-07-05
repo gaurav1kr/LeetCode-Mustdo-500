e308f2b282361fa8f4e35da498d698bbb8474949
Gaurav Kumar <sinha4545@gmail.com>
2023-07-06 02:03:19 +0530

    Heap Sort - 494

---
+++ Heap-Sort-494.cpp
@@ -0,0 +1,40 @@
+#include<vector>
+using namespace std;
+class Solution
+{
+public:
+    void heapify(vector<int>& arr, int N, int i)
+    {
+        int largest = i;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+
+        if (l < N && arr[l] > arr[largest])
+            largest = l;
+
+        if (r < N && arr[r] > arr[largest])
+            largest = r;
+
+
+        if (largest != i)
+        {
+            swap(arr[i], arr[largest]);
+            heapify(arr, N, largest);
+        }
+    }
+
+    vector<int> sortArray(vector<int> arr)
+    {
+        int N = arr.size();
+        for (int i = N / 2 - 1; i >= 0; i--)
+            heapify(arr, N, i);
+
+        for (int i = N - 1; i > 0; i--)
+        {
+            swap(arr[0], arr[i]);
+            heapify(arr, i, 0);
+        }
+        return arr;
+    }
+
+};
