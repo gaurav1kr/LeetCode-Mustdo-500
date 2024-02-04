2315f465a5b2de9206698e30d5c64776c6ef6f19
Gaurav Kumar <sinha4545@gmail.com>
2024-02-04 12:36:45 +0530

    overlapping_calendar-470

---
+++ overlapping_calendar-470.cpp
@@ -0,0 +1,33 @@
+class MyCalendar
+{
+    map<int, int> events;
+public:
+    MyCalendar()
+    {
+
+    }
+
+    bool book(int start, int end)
+    {
+        auto next = events.upper_bound(start);
+        if (next != events.end() && (*next).second < end)return false;
+        events.insert({ end,start });
+        return true;
+    }
+};class MyCalendar
+{
+    map<int, int> events;
+public:
+    MyCalendar()
+    {
+
+    }
+
+    bool book(int start, int end)
+    {
+        auto next = events.upper_bound(start);
+        if (next != events.end() && (*next).second < end)return false;
+        events.insert({ end,start });
+        return true;
+    }
+};
