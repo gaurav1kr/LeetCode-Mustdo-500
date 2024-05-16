5b9dddb47263df4ceb37854fc20b09787e502791
Gaurav Kumar <sinha4545@gmail.com>
2024-05-16 23:57:54 +0530

    sort_k_sorted_linked_list_q24

--- sort_k_sorted_linked_list_q24.cpp
+++ sort_k_sorted_linked_list_q24.cpp
@@ -1,13 +1,12 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+struct ListNode 
+{
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
 struct compare
 {
 		bool operator()(ListNode *const & a , ListNode *const & b)
@@ -46,3 +45,26 @@ public:
 			return dummy_node->next ;    
     }
 };
+
+// Time complexiety :- O(n*log(k)).
+
+//Logic :- 
+1. The compare struct defines a functor to compare two ListNode pointers. It compares the val member of the ListNode objects pointed to by a and b.
+
+2. The Solution class contains the mergeKLists function, which takes a vector of pointers to ListNode objects representing k sorted linked lists.
+
+3. It first checks if the input vector lists is empty. If so, it returns NULL.
+
+4. It initializes a priority queue pq of ListNode pointers. The priority queue is configured to use the custom comparison operator defined in the compare struct.
+
+5. It iterates through each linked list in the input vector lists. If the linked list is not empty (ll), it pushes the head of the linked list onto the priority queue.
+
+6. It creates a dummy node and initializes tail to point to it.
+
+7. It enters a loop that continues until the priority queue is empty.
+
+8. Inside the loop, it pops the top element (which has the smallest value according to the custom comparison function) from the priority queue and appends it to the end of the merged list.
+
+9. If the popped node has a next node, it pushes the next node onto the priority queue.
+
+10. Finally, it returns the next node of the dummy node, which is the head of the merged linked list.
