a347bab1d7eebdd5c4abcc73ccf716035ad3966f
Gaurav Kumar <sinha4545@gmail.com>
2024-08-16 14:48:15 +0530

    array_nesting_458

--- array_nesting_458.cpp
+++ array_nesting_458.cpp
@@ -20,3 +20,39 @@ public:
         return max_len;
     }
 };
+//Recursive
+class Solution {
+    int max ;
+public:
+    Solution()
+    {
+        max = INT_MIN ;
+    }
+    void dfs(int elem , vector<int> nums , vector<bool> visited , int & c)
+    {
+        if (visited[elem])
+            return ;
+ 
+        if (c > max)
+        {
+            max = c ;
+        }
+ 
+        visited[elem] = true ;
+        c++ ;
+        dfs(nums[elem], nums , visited , c) ;
+        c-- ;
+        visited[elem] = false ;
+    }
+    int arrayNesting(vector<int>& nums) 
+    {
+        int size_num = sizeof(nums) ;
+        vector<bool> visited(size_num,false) ;
+        int count = 1 ;
+        for(auto & elem:nums)
+        {
+            dfs(elem , nums , visited , count);
+        }    
+        return max ;
+    }
+};
