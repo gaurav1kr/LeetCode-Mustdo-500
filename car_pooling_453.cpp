15d6b28093e82fc9fc8aed3ec8474cf5bd1141ef
Gaurav Kumar <sinha4545@gmail.com>
2024-08-20 12:58:24 +0530

    car_pooling_453

--- car_pooling_453.cpp
+++ car_pooling_453.cpp
@@ -5,9 +5,13 @@ public:
   {
     int stops[1001] = { };
   for (auto t:trips)
-      stops[t[1]] += t[0], stops[t[2]] -= t[0];
+      {
+	stops[t[1]] += t[0], stops[t[2]] -= t[0];
+      }
     for (auto i = 0; capacity >= 0 && i < 1001; ++i)
-      capacity -= stops[i];
+      {
+	capacity -= stops[i];
+      }
     return capacity >= 0;
   }
 };
