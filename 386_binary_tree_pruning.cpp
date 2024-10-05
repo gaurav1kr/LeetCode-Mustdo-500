fa7c9b9aedf790e666e5f745d24da6ae825253f6
Gaurav Kumar <sinha4545@gmail.com>
2024-10-05 16:11:34 +0530

    386_binary_tree_pruning

---
+++ 386_binary_tree_pruning.cpp
@@ -0,0 +1,22 @@
+class Solution 
+{
+public:
+    int solve(TreeNode* &root)
+    {
+        if(!root)return 0;
+        int  sum = 0;
+        sum = root->val + solve(root->left) + solve(root->right);
+        if(sum == 0)
+		{
+            root = NULL;
+        }
+        return sum;
+    }
+    TreeNode* pruneTree(TreeNode* root) 
+    {
+       solve(root);
+       return root;   
+    }
+};
+
+//TC :- O(N)
