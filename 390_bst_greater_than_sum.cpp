ea89a7a3b68b088095a65f9e4a2b9e57cb2dcf1f
Gaurav Kumar <sinha4545@gmail.com>
2024-10-05 13:45:25 +0530

    390_bst_greater_than_sum

--- 390_bst_greater_than_sum.cpp
+++ 390_bst_greater_than_sum.cpp
@@ -1,8 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution 
 {
 public:
-    TreeNode* bstToGst(TreeNode* root) 
-    {
-        
-    }
+	void bstToGstUtil(TreeNode *root, int & temp)
+	{
+		if(!root)
+		{
+			return ;
+		}
+		bstToGstUtil(root->right , temp) ;
+		root->val += temp ;
+        	temp = root->val ;
+		bstToGstUtil(root->left , temp) ;
+	}
+   
+ 	TreeNode* bstToGst(TreeNode* root) 
+    	{
+       		int temp = 0;
+	   	bstToGstUtil(root, temp);
+       		return root ;
+    	}
 };
+
+
+// TC :- O(N)
+// SC :- O(N)
