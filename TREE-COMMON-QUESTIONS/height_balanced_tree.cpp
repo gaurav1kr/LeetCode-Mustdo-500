c782d76414066cf10e357e2e925aa96afd56fe9e
Gaurav Kumar <sinha4545@gmail.com>
2025-01-26 12:23:52 +0530

    TREE-COMMON-QUESTIONS/height_balanced_tree

---
+++ TREE-COMMON-QUESTIONS/height_balanced_tree.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution 
+{
+public:
+    bool isBalanced(TreeNode* root) 
+    {
+        bool balanced = true;
+        dfs(root, balanced);
+        return balanced;
+    }
+
+    int dfs(TreeNode* root, bool& balanced) 
+    {
+        if (root == nullptr ) 
+	{
+            return 0;
+        }
+
+        int left = dfs(root->left, balanced);
+        int right = dfs(root->right, balanced);
+
+        if (abs(left - right) > 1) 
+	{
+            balanced = false;
+        }
+
+        return 1 + max(left, right);
+    }
+};
