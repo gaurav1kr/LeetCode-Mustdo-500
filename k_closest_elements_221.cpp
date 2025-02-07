8f43dbaa6ffba39986a4badaf4133bc25164ac11
Gaurav Kumar <sinha4545@gmail.com>
2025-02-07 21:37:28 +0530

    k_closest_elements_221

---
+++ k_closest_elements_221.cpp
@@ -0,0 +1,26 @@
+class Solution 
+{
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) 
+    {
+        priority_queue<pair<int,int>> p;
+        vector<int>v;
+        for(auto i:arr)
+        {
+            int n=abs(x-i);
+            p.push({n,i});
+
+            if(p.size()>k)
+            {
+                 p.pop();
+            }
+        }
+        while(p.size()>0)
+        {
+            v.push_back(p.top().second);
+            p.pop();
+        }
+        sort(v.begin(),v.end());
+        return v;
+    }
+};
