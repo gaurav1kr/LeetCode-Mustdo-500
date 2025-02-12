5d58ea68ade0e9d5862708a8119fb07dd0cfafcc
Gaurav Kumar <sinha4545@gmail.com>
2025-02-12 05:48:14 +0530

    modify

--- push_in_lc.sh
+++ push_in_lc.sh
@@ -1,4 +1,4 @@
-file=`ls -l /mnt/c/Users/Dell/Downloads/*.md`
-cp /mnt/c/Users/Dell/Downloads/$file $1_$file 
+file=`ls -1 /mnt/c/Users/Dell/Downloads/*.md`
+cp /mnt/c/Users/Dell/Downloads/$file "$1"_"$file" 
 ../push_new_code.sh
 rm -rf /mnt/c/Users/Dell/Downloads/*.md
