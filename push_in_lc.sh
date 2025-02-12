73b449f381526e86a085dbcb4abe23d999e6a1b3
Gaurav Kumar <sinha4545@gmail.com>
2025-02-12 05:54:08 +0530

    modify

--- push_in_lc.sh
+++ push_in_lc.sh
@@ -1,4 +1,5 @@
-file=`ls -1 /mnt/c/Users/Dell/Downloads/*.md`
+file=`ls -1 /mnt/c/Users/Dell/Downloads/*.md | awk -F "/" '{print $7}'`
+echo "Pushing...." $file
 cp /mnt/c/Users/Dell/Downloads/$file "$1"_"$file" 
 ../push_new_code.sh
 rm -rf /mnt/c/Users/Dell/Downloads/*.md
